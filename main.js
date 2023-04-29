04837919d8dd73284606b2c17035d7a103189fd6
Chen Xiang Wei <106435999+hsiu-chan@users.noreply.github.com>
2023-04-29 23:05:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       xiang
-// @match        https://newacademic.tmu.edu.tw/mainframe.aspx
+// @match        https://newacademic.tmu.edu.tw/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=edu.tw
 // @grant        none
 // @license     MIT
